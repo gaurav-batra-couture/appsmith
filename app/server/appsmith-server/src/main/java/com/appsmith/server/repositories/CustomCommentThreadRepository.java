fc60b194e3348ace4a24aed0e19c76e111829428
Nayan <nayan@appsmith.com>
2021-07-06 19:02:08 +0600

    -added API to and template for unsubscribe  comment thread

--- app/server/appsmith-server/src/main/java/com/appsmith/server/repositories/CustomCommentThreadRepository.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/repositories/CustomCommentThreadRepository.java
@@ -11,5 +11,6 @@ import java.util.Set;
 public interface CustomCommentThreadRepository extends AppsmithRepository<CommentThread> {
     Flux<CommentThread> findByApplicationId(String applicationId, AclPermission permission);
     Mono<UpdateResult> addToSubscribers(String threadId, Set<String> usernames);
+    Mono<UpdateResult> removeSubscriber(String threadId, String username);
     Mono<Long> countUnreadThreads(String applicationId, String userEmail);
 }
